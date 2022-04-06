9a1d33992422cd332035bdc9828694a3607b732d
tmedcount <tmedcount@gmail.com>
2022-04-06 11:42:36 +0900

    2022-04-06, 2차 배포

---
+++ current.sql
@@ -0,0 +1,81 @@
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 10.4.22-MariaDB : Database - textBoard
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`textBoard` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `textBoard`;
+
+/*Table structure for table `article` */
+
+DROP TABLE IF EXISTS `article`;
+
+CREATE TABLE `article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `title` char(200) NOT NULL,
+  `body` text NOT NULL,
+  `memberId` int(10) unsigned NOT NULL,
+  `boardId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `article` */
+
+insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberId`,`boardId`) values 
+(1,'2022-04-06 11:35:14','2022-04-06 11:35:14','1강, SQL SELECT','# SELECT\r\n- 데이터 조회 기능\r\n\r\n# SELECT 예시\r\n```\r\nSELECT `column-name`,\r\nFROM `table_name`\r\nWHERE `column_name` > 1 # 조건\r\n```',1,3);
+
+/*Table structure for table `board` */
+
+DROP TABLE IF EXISTS `board`;
+
+CREATE TABLE `board` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `name` char(20) NOT NULL,
+  `code` char(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `board` */
+
+insert  into `board`(`id`,`regDate`,`updateDate`,`name`,`code`) values 
+(1,'2022-04-05 17:45:55','2022-04-05 17:45:55','공지사항','notice'),
+(2,'2022-04-05 17:45:55','2022-04-05 17:45:55','자유','free'),
+(3,'2022-04-06 11:30:11','2022-04-06 11:30:11','SQL','it');
+
+/*Table structure for table `member` */
+
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `loginId` char(30) NOT NULL,
+  `loginPw` varchar(50) NOT NULL,
+  `name` char(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `member` */
+
+insert  into `member`(`id`,`regDate`,`updateDate`,`loginId`,`loginPw`,`name`) values 
+(1,'2022-04-06 11:30:11','2022-04-06 11:30:11','admin','admin','강호성');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
